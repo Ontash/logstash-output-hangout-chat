6ab1fa17dba4f947cb53b57e31cc3ca878dd6893
Vizkrig <mail.vishnupk@gmail.com>
2019-01-31 12:08:41 +0530

    change spec for hangout chat

--- spec/outputs/hangout-chat_spec.rb
+++ spec/outputs/hangout-chat_spec.rb
@@ -1,12 +1,12 @@
 # encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
-require "logstash/outputs/stride"
+require "logstash/outputs/hangout-chat"
 require "logstash/codecs/plain"
 require "logstash/event"
 
-describe LogStash::Outputs::Stride do
+describe LogStash::Outputs::HangoutChat do
   let(:sample_event) { LogStash::Event.new }
-  let(:output) { LogStash::Outputs::Stride.new }
+  let(:output) { LogStash::Outputs::HangoutChat.new }
 
   before do
     output.register
